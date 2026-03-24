9ad5837f31fe744299555b8e2a060486dd77a8f6
RockieRaheem <Kamwangaraheem2050@gmail.com>
2026-03-24 15:45:02 +0300

    Implement soft deletes on items to preserve historical data integrity

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Item Model
@@ -18,7 +19,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class Item extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * The table associated with the model.
